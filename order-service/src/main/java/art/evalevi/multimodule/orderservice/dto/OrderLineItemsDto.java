46543c2d5853ccdc6584cf6f6b0da2bbaadfdceb
neighborstan <dropweb.post@gmail.com>
2023-02-12 22:31:52 +0600

    Added endpoint test for order service

--- order-service/src/main/java/art/evalevi/multimodule/orderservice/dto/OrderLineItemsDto.java
+++ order-service/src/main/java/art/evalevi/multimodule/orderservice/dto/OrderLineItemsDto.java
@@ -1,6 +1,7 @@
 package art.evalevi.multimodule.orderservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderLineItemsDto {
     private Long id;
     private String skuCode;
