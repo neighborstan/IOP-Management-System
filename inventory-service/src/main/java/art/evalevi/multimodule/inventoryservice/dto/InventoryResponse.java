1a7aabe1c8f6f4e9508800dec973418086728fc9
neighborstan <dropweb.post@gmail.com>
2023-02-14 21:06:00 +0600

    Added communication between order-service and inventory-service.

---
+++ inventory-service/src/main/java/art/evalevi/multimodule/inventoryservice/dto/InventoryResponse.java
@@ -0,0 +1,15 @@
+package art.evalevi.multimodule.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
