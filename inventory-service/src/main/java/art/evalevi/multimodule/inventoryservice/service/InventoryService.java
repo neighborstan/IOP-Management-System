e0380d40aa304e8e074e4661fcc1bc2719ccadcc
neighborstan <dropweb.post@gmail.com>
2023-02-22 23:18:32 +0600

    Added Resilience4j Circuit Breaker

--- inventory-service/src/main/java/art/evalevi/multimodule/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/art/evalevi/multimodule/inventoryservice/service/InventoryService.java
@@ -1,29 +1,38 @@
 package art.evalevi.multimodule.inventoryservice.service;
 
 import art.evalevi.multimodule.inventoryservice.dto.InventoryResponse;
+import art.evalevi.multimodule.inventoryservice.model.Inventory;
 import art.evalevi.multimodule.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
-
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCodes) {
-        return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
-                .map(inventory ->
-                        InventoryResponse.builder()
-                                .skuCode(inventory.getSkuCode())
-                                .isInStock(inventory.getQuantity() > 0)
-                                .build()
-                ).toList();
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(this::mapInventoryAndThrowErrorIfBadluck)
+                .toList();
+    }
+
+    private InventoryResponse mapInventoryAndThrowErrorIfBadluck(Inventory inventory) {
+        return InventoryResponse.builder()
+                .skuCode(inventory.getSkuCode())
+                .isInStock(inventory.getQuantity() > 0)
+                .build();
     }
 }
