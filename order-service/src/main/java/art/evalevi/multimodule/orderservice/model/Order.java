91b6f501b30da58bc6158da0cb4dfe70d94d2c54
neighborstan <dropweb.post@gmail.com>
2023-02-23 19:32:07 +0600

    Added distributed tracing. Spring Sleuth. Zipkin.

--- order-service/src/main/java/art/evalevi/multimodule/orderservice/model/Order.java
+++ order-service/src/main/java/art/evalevi/multimodule/orderservice/model/Order.java
@@ -1,8 +1,8 @@
 package art.evalevi.multimodule.orderservice.model;
 
-import jakarta.persistence.*;
 import lombok.*;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
--- order-service/src/main/java/art/evalevi/multimodule/orderservice/service/OrderService.java
+++ order-service/src/main/java/art/evalevi/multimodule/orderservice/service/OrderService.java
@@ -7,12 +7,15 @@ import art.evalevi.multimodule.orderservice.model.Order;
 import art.evalevi.multimodule.orderservice.model.OrderLineItems;
 import art.evalevi.multimodule.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -22,6 +25,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final Tracer tracer;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,24 +42,32 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
-                .uri("http://inventory-service/api/inventory",
-                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
-                .retrieve()
-                .bodyToMono(InventoryResponse[].class)
-                .block();
 
-        boolean allProductsInStock = false;
-        if (inventoryResponses != null) {
-            allProductsInStock = Arrays.stream(inventoryResponses)
+        Span inventoryServiceLookup = tracer.nextSpan().name("InventoryServiceLookup");
+
+        try (Tracer.SpanInScope isLookup = tracer.withSpan(inventoryServiceLookup.start())) {
+
+            inventoryServiceLookup.tag("call", "inventory-service");
+
+            // Call Inventory Service, and place order if product is in stock
+            InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                    .uri("http://inventory-service/api/inventory",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            boolean allProductsInStock = Arrays.stream(Objects.requireNonNull(inventoryResponseArray))
                     .allMatch(InventoryResponse::isInStock);
-        }
 
-        if (allProductsInStock) {
-            orderRepository.save(order);
-            return "Order Placed Successfully";
-        } else {
-            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+            if (allProductsInStock) {
+                orderRepository.save(order);
+                return "Order Placed Successfully";
+            } else {
+                throw new IllegalArgumentException("Product is not in stock, please try again later");
+            }
+        } finally {
+            inventoryServiceLookup.end();
         }
     }
 
